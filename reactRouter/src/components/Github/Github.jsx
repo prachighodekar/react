c221d70011f5e0f339b075d8041909d71684fef6
prachighodekar <prachighodekar98@gmail.com>
2025-09-13 17:36:40 -0400

    ReactRouter

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState()
+    // useEffect(()=>{
+    //    fetch("https://api.github.com/users/prachighodekar")
+    //    .then(response => response.json())
+    //    .then(data => {
+    //     console.log(data)
+    //     setData(data)
+    //    })
+    // },[])
+  return (
+    <div className='text-center m-4 bg-gray-600 p-4 text-white text-3xl'>
+      Github Followers: {data?.followers}
+      <img src = {data?.avatar_url} alt='Git Profile'/>
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/prachighodekar')
+    return response.json()
+}
